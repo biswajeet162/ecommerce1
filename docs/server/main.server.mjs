9533f916a1e1b6e9e989e3aa8b02bbfacc4e9961
Biswajeet <biswajeet163@gmail.com>
2024-09-02 20:25:25 +0530

    angulae.json file

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4C6MNXLY.mjs";import"./chunk-YKBJMKVP.mjs";import"./chunk-NKL6DVTV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2MT3MY44.mjs";import"./chunk-YKBJMKVP.mjs";import"./chunk-NKL6DVTV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
